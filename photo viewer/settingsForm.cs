75e5e27eb849a890ac2258d04ad263afad3b4a88
mateidev <serbanmatei572@gmail.com>
2025-06-24 14:35:58 +0300

    Added a settings window

---
+++ photo viewer/settingsForm.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace photo_viewer
+{
+    public partial class settingsForm : Form
+    {
+
+        string path = Properties.Settings.Default.FolderPath;
+
+        public settingsForm()
+        {
+            Panel sidePanel = new Panel
+            {
+                Dock = DockStyle.Left,
+                Width = 130,
+                BackColor = Color.LightGray
+            };
+
+            Label pathBtn = new Label
+            {
+                Text = "Path",
+            };
+
+            this.Controls.Add(sidePanel);
+            sidePanel.Controls.Add(pathBtn);
+            InitializeComponent();
+        }
+    }
+}
